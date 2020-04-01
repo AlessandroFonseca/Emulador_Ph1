8a52cc83b6d4b8d53f22fdea76c9aa610309c7d0
alessandrofonseca <alessandrofonseca@192.168.0.3>
2020-04-01 20:29:35 -0300

    Commit inicial do projeto

---
+++ Sources/main.c
@@ -0,0 +1,37 @@
+/**
+ * @addtogroup main
+ *
+ * @details Este arquivo contém a funcao principal do programa
+ *
+ * @details <b>Histórico</b>
+ * \n 1-04-2020 Alessandro Fonseca Criacao do arquivo
+ *
+ */
+
+/** @{ */
+#include <stdio.h>
+
+/**
+ * @brief funcao principal do programa
+ *
+ * @details Esta funcao e chamada quando inicia o programa
+ *
+ * @details <b>Historico</b>
+ * \n 01-04-2020 Alessandro Fonseca Criacao da funcao
+ *
+ */
+int main(int argc, char** argv) {
+
+	printf("Hello World\n");
+	return 0;
+
+}
+
+
+
+
+
+
+
+/** @} */
+
