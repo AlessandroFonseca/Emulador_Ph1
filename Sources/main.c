d95deae617b0bb90c9909e841005acbded8fdb64
alessandrofonseca <alessandrofonseca@192.168.0.4>
2020-04-09 22:12:48 -0300

    adicionado o printf nas instruções, criado o Cont para as instruções executadas, feito a formatação da saída para mostrar o contador de instruções os registradores e a memória modificada criado vetor sombra para rastrear o conteudo da memória modificada

--- Sources/main.c
+++ Sources/main.c
@@ -13,6 +13,7 @@
 
 
 int memoria[256];
+int sombra [256];
 int pc;
 int ri;
 int rdm;
@@ -51,6 +52,7 @@ int main(int argc, char** argv) {
 
 		fscanf( arq ,"%x %x", &endereco, &dado);
 		memoria[endereco] = dado;
+		sombra [endereco] = dado;
 		printf("%x = %x\n", endereco,dado);
 	}
 
@@ -72,154 +74,215 @@ int main(int argc, char** argv) {
  */
 void cpu(void){
 
-printf(" Funcao cpu\n ");
+	int i;
+	int cont=0;
+	printf("Funcao cpu\n");
 
-pc=0;// inicializa o pc..
+	pc=0;// inicializa o pc..
 
-rem=pc;
-rdm= memoria[rem];// o indice do vetor é o endereço da nossa memória
-pc=pc+1;
-ri=rdm;
+	while(1)
+	{
+		rem=pc;
+		rdm= memoria[rem];// o indice do vetor é o endereço da nossa memória
+		pc=pc+1;
+		ri=rdm;
 
-//[0000]0000 = 0x00
-//[0111]0000 = 0x70
-//[1111]0000 = 0xF0
-if(ri!=0x00 && ri!=0x70 && ri!=0xf0){ // se a instrução ocupa 01 endereço de memória
-rem=pc;
-rdm= memoria[rem];
-pc=pc+1;
-}
+		//[0000]0000 = 0x00
+		//[0111]0000 = 0x70
+		//[1111]0000 = 0xF0
+		if(ri!=0x00 && ri!=0x70 && ri!=0xf0){ // se a instrução ocupa 01 endereço de memória
+			rem=pc;
+			rdm= memoria[rem];
+			pc=pc+1;
+		}
 
-if(ri==0x00){
-//ir para passo zero
-}
-
-
-//[0001]0000 = 0x10
-if(ri==0x10){ //LDR
-	rem=rdm;
-	rdm=memoria[rem];
-	ac= rdm;
-	//ir para zero
-}
+		if(ri==0x00){ //NOP
+		//ir para passo zero
+			printf("NOP;\n");
+			cont++;
+			continue;
+		}
 
 
-//[0010]0000 = 0x20
-if(ri==0x20){ //STR
-	rem=rdm;
-	rdm= ac;
-	memoria[rem]=rdm;
-	//ir para zero
-}
+		//[0001]0000 = 0x10
+		if(ri==0x10){ //LDR
+			printf("LDR %02x;\n", rdm);
+			rem=rdm;
+			rdm=memoria[rem];
+			ac= rdm;
+			//ir para zero
+			cont++;
+			continue;
+		}
 
 
-//[0011]0000 = 0x30
-if(ri==0x30){ //ADD
-	rem=rdm;
-	rdm=memoria[rem];
-	ac= ac+rdm;
-	//ir para zero
-}
+		//[0010]0000 = 0x20
+		if(ri==0x20){ //STR
+			printf("STR %02x;\n", rdm);
+			rem=rdm;
+			rdm= ac;
+			memoria[rem]=rdm;
+			//ir para zero
+			cont++;
+			continue;
+		}
 
 
-//[0100]0000 = 0x40
-if(ri==0x40){ //SUB
-	rem=rdm;
-	rdm=memoria[rem];
-	ac= ac-rdm;
-	//ir para zero
-}
+		//[0011]0000 = 0x30
+		if(ri==0x30){ //ADD
+			printf("ADD %02x;\n", rdm);
+			rem=rdm;
+			rdm=memoria[rem];
+			ac= ac+rdm;
+			//ir para zero
+			cont++;
+			continue;
+		}
 
 
-//[0101]0000 = 0x50
-if(ri==0x50){ //MUL
-	rem=rdm;
-	rdm=memoria[rem];
-	ac= ac*rdm;
-	//ir para zero
-}
-
-//[0110]0000 = 0x60
-if(ri==0x60){ //DIV
-	rem=rdm;
-	rdm=memoria[rem];
-	ac= ac/rdm;
-	//ir para zero
-}
-
-//[0101]0000 = 0x70
-if(ri==0x70){ //NOT
-	rem=rdm;
-	rdm=memoria[rem];
-	ac= ~ac;
-	//ir para zero
-}
-
-//[1000]0000 = 0x80
-if(ri==0x80){ //AND
-	rem=rdm;
-	rdm=memoria[rem];
-	ac= ac & memoria[rdm];
-	//ir para zero
-}
+		//[0100]0000 = 0x40
+		if(ri==0x40){ //SUB
+			printf("SUB %02x;\n", rdm);
+			rem=rdm;
+			rdm=memoria[rem];
+			ac= ac-rdm;
+			//ir para zero
+			cont++;
+			continue;
+		}
 
 
-//[1001]0000 = 0x90
-if(ri==0x90){ //OR
-	rem=rdm;
-	rdm=memoria[rem];
-	ac= ac | memoria[rem];
-	//ir para zero
-}
+		//[0101]0000 = 0x50
+		if(ri==0x50){ //MUL
+			printf("MUL %02x;\n", rdm);
+			rem=rdm;
+			rdm=memoria[rem];
+			ac= ac*rdm;
+			//ir para zero
+			cont++;
+			continue;
+		}
+
+		//[0110]0000 = 0x60
+		if(ri==0x60){ //DIV
+			printf("DIV %02x;\n", rdm);
+			rem=rdm;
+			rdm=memoria[rem];
+			ac= ac/rdm;
+			//ir para zero
+			cont++;
+			continue;
+		}
+
+		//[0101]0000 = 0x70
+		if(ri==0x70){ //NOT
+			printf("NOT;\n");
+			ac= ~ac;
+			//ir para zero
+			cont++;
+			continue;
+		}
+
+		//[1000]0000 = 0x80
+		if(ri==0x80){ //AND
+			printf("AND %02x;\n", rdm);
+			rem=rdm;
+			rdm=memoria[rem];
+			ac= ac & memoria[rdm];
+			//ir para zero
+			cont++;
+			continue;
+		}
 
 
-//[1010]0000 = 0xA0;
-if(ri==0xA0){ //XOR
-	rem=rdm;
-	rdm=memoria[rem];
-	ac= ac ^ memoria[rdm];
-	//ir para zero
-}
-
-//[1011]0000 = 0xB0
-if(ri==0xB0){ //JMP
-	pc= rdm;
-	//ir para zero
-}
+		//[1001]0000 = 0x90
+		if(ri==0x90){ //OR
+			printf("OR %02x;\n", rdm);
+			rem=rdm;
+			rdm=memoria[rem];
+			ac= ac | memoria[rem];
+			//ir para zero
+			cont++;
+			continue;
+		}
 
 
-//[1100]0000 = 0xC0
-if(ri==0xC0){ //JEQ
-	if(ac==0){
-		pc=rdm;
+		//[1010]0000 = 0xA0;
+		if(ri==0xA0){ //XOR
+			printf("XOR %02x;\n", rdm);
+			rem=rdm;
+			rdm=memoria[rem];
+			ac= ac ^ memoria[rdm];
+			//ir para zero
+			cont++;
+			continue;
+		}
+
+		//[1011]0000 = 0xB0
+		if(ri==0xB0){ //JMP
+			printf("JMP %02x;\n", rdm);
+			pc= rdm;
+			//ir para zero
+			cont++;
+			continue;
+		}
+
+
+		//[1100]0000 = 0xC0
+		if(ri==0xC0){ //JEQ
+			printf("JEQ %02x;\n", rdm);
+			if(ac==0){
+				pc=rdm;
+			}
+			//ir para zero
+			cont++;
+			continue;
+		}
+
+		//[1101]0000 = 0xD0
+		if(ri==0xD0){ //JG
+			printf("JG %02x;\n", rdm);
+			if(ac>0){
+				pc=rdm;
+			}
+			//ir para zero
+			cont++;
+			continue;
+		}
+
+		//[1110]0000 = 0xE0
+		if(ri==0xE0){ //JL
+			printf("JL %02x;\n", rdm);
+			if(ac<0){
+				pc=rdm;
+			}
+			//ir para zero
+			cont++;
+			continue;
+		}
+
+		//[1111]0000 = 0xF0
+		if(ri==0xF0){ //HLT
+			printf("HLT;\n");
+			cont++;
+			break;
+			//ir para Halted
+		}
+
+	}//while...
+	printf("\n");
+	printf("%d instructions executed\n", cont);
+	printf("\n");
+	printf("Registers:\n");
+	printf("AC: %02x\n", ac);
+	printf("PC: %02x\n", pc);
+	printf("\n");
+	printf("Memory:\n");
+	for(i=128;i<255;i++){
+		if(sombra[i] != memoria[i]){
+			printf("%02x %02x\n", i,memoria[i]);
+		}
 	}
-	//ir para zero
-}
-
-//[1101]0000 = 0xD0
-if(ri==0xD0){ //JG
-	if(ac>0){
-		pc=rdm;
-	}
-	//ir para zero
-}
-
-//[1110]0000 = 0xE0
-if(ri==0xE0){ //JL
-	if(ac<0){
-		pc=rdm;
-	}
-	//ir para zero
-}
-
-//[1111]0000 = 0xF0
-if(ri==0xF0){ //HLT
-
-
-	//ir para Halted
-}
-
-
 
 }
 
